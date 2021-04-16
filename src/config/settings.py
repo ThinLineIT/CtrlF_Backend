b599e0d809332d7df5d0f644df0888b3c71d954b
TaeHyoungKwon <kth5604@gmail.com>
2021-04-17 00:55:32 +0900

    add STATIC_ROOT

--- src/config/settings.py
+++ src/config/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
