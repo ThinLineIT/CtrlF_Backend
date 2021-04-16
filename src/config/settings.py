f2d6bc7f9983482d772ab6876927519de7a8edb2
TaeHyoungKwon <kth5604@gmail.com>
2021-04-17 00:11:35 +0900

    add allowed_hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nhf51zwy50y#xwte^vj!mf-_@n&k&c6q+#4%2izibb%*0q(o6h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
