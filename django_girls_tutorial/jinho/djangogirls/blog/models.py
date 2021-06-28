ce441ec7250712089cebe604f7bf5faf432ef94a
JinHoooooou <jinho4744@naver.com>
2021-06-28 14:40:43 +0900

    modify for coding convention

--- django_girls_tutorial/jinho/djangogirls/blog/models.py
+++ django_girls_tutorial/jinho/djangogirls/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
