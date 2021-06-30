baf465292e1205be6bed01ca09e72b3c66f428f2
TaeHyoungKwon <kth5604@gmail.com>
2021-07-01 08:01:46 +0900

    add solution on post detail

--- django_girls_tutorial/step2/jinho/djangogirls/blog/api/views.py
+++ django_girls_tutorial/step2/jinho/djangogirls/blog/api/views.py
@@ -1,3 +1,5 @@
+from http.client import NOT_FOUND, OK
+
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -7,3 +9,23 @@ from blog.models import Post
 def retrieve_post_list(request):
     # 해당 view 함수를 구현하시오.
     pass
+
+
+def retrieve_post_detail(request, id):
+    try:
+        post = Post.objects.get(published_date__lte=timezone.now())
+    except Post.DoesNotExist:
+        return JsonResponse(status=NOT_FOUND, data={"message": "Post를 찾을 수 없습니다"})
+    else:
+        return JsonResponse(
+            status=OK,
+            data={
+                "post": {
+                    "author": post.author.id,
+                    "title": post.title,
+                    "text": post.text,
+                    "created_date": post.created_date,
+                    "published_data": post.published_date,
+                }
+            },
+        )
