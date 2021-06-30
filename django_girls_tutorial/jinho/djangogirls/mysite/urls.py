c89ea0b0b4b9ad25d3c4500038698e9e28f9d6c5
JinHoooooou <jinho4744@naver.com>
2021-06-30 13:12:55 +0900

    add logout function

--- django_girls_tutorial/jinho/djangogirls/mysite/urls.py
+++ django_girls_tutorial/jinho/djangogirls/mysite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
