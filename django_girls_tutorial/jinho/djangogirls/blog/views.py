f42134c5de08bcc27add1548d8633c0d947a0792
JinHoooooou <jinho4744@naver.com>
2021-06-30 11:43:31 +0900

    remove published_date for draft_list

--- django_girls_tutorial/jinho/djangogirls/blog/views.py
+++ django_girls_tutorial/jinho/djangogirls/blog/views.py
@@ -21,7 +21,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -36,7 +35,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
