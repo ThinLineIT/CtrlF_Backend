1489af51082417971e6db19aa6c8901be1e1e8a5
JinHoooooou <jinho4744@naver.com>
2021-06-30 13:07:55 +0900

    add login function

--- django_girls_tutorial/jinho/djangogirls/mysite/urls.py
+++ django_girls_tutorial/jinho/djangogirls/mysite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
 ]
