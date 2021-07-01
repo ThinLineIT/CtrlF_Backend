72c7f2d1862b29a2b97ebd4b234a03f29e9320ae
JinHoooooou <jinho4744@naver.com>
2021-07-02 08:41:31 +0900

    remove email argument in _create_author method

--- django_girls_tutorial/step2/jinho/djangogirls/blog/tests.py
+++ django_girls_tutorial/step2/jinho/djangogirls/blog/tests.py
@@ -9,15 +9,15 @@ from .models import Post
 
 class TestPostMixin:
     def setUp(self):
-        self.author = self._create_author(username="thkwon", password="test_password", email="thkwon@ctrlf.com")
+        self.author = self._create_author(username="thkwon", password="test_password")
 
     @staticmethod
     def _create_post(author, title, text):
         return Post.objects.create(author=author, title=title, text=text)
 
     @staticmethod
-    def _create_author(username, password, email):
-        return User.objects.create_superuser(username=username, password=password, email=email)
+    def _create_author(username, password):
+        return User.objects.create_superuser(username=username, password=password)
 
 
 class TestPostList(TestPostMixin, TestCase):
