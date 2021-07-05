549bb081633da43645b217d2e693bbe222ebb29b
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2021-07-05 18:05:16 +0900

    add post api

--- django_girls_tutorial/step2/jingu/djangogirls/blog/api/views.py
+++ django_girls_tutorial/step2/jingu/djangogirls/blog/api/views.py
@@ -3,84 +3,103 @@ from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 
 from blog.models import Post
+from django.contrib.auth.models import User
+from blog.form import PostForm
+
+from http.client import NOT_FOUND, OK, CREATED
+import json
 
 
 def retrieve_post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
 
-    result = {}
-    result["posts"] = []
+    result = [
+        {
+            "title": post.title,
+            "text": post.text,
+            "author": post.author.id,
+            "created_date": post.created_date,
+            "published_date": post.published_date,
+        }
+        for post in posts
+    ]
 
-    for post in posts:
-        temp_post = {}
-        temp_post["title"] = post.title
-        temp_post["text"] = post.text
-        temp_post["author"] = post.author.id
-        temp_post["created_date"] = post.created_date
-        temp_post["published_date"] = post.published_date
-        result["posts"].append(temp_post)
-
-    return JsonResponse(result)
+    return JsonResponse({"posts": result}, status=OK)
 
 
 def retrieve_post_detail(request, id):
-    """
-    해당 view 함수를 구현하시오.
+    try:
+        post = Post.objects.get(id=id)
+    except:
+        return JsonResponse({"message": "Post를 찾을 수 없습니다"}, status=NOT_FOUND)
 
-    Case 1: 성공하는 경우,
-
-    응답 JSON
-    {
-        "post": {
-            "author": 1,
-            "title": "test title",
-            "text": "test text",
-            "created_date": "2021-01-01T00:00:00" # 포맷은 다를 수 있음
-            "published_date": "2021-01-02T01:00:00" # 포맷은 다를 수 있음
-        }
-    }
-    상태코드 200
-
-    Case 2: 실패하는 경우, - 검색하려는 Post가 존재하지 않음
-
-    응답 JSON
-    {
-        "message": "Post를 찾을 수 없습니다"
-    }
-    상태코드 404
-    """
+    return JsonResponse(
+        {
+            "post": {
+                "author": post.author.id,
+                "title": post.title,
+                "text": post.text,
+                "created_date": post.created_date,
+                "published_date": post.published_date,
+            }
+        },
+        status=OK,
+    )
 
 
 @require_http_methods(["POST"])
 def create_post(request):
-    """
-    해당 view 함수를 구현하시오.
+    try:
+        user = User.objects.get(id=request.POST["author"])
+    except:
+        return JsonResponse(
+            {
+                "message": "author를 찾을 수 없습니다.",
+            },
+            status=NOT_FOUND,
+        )
 
-    Case 1: 성공하는 경우,
-    상태코드 200
-
-    Case 2: 실패하는 경우, - author가 존재하지 않음
-    상태코드 404
-
-    자세한 조건은 테스트를 확인해보고 파악하자
-
-    FYI, request.POST 활용
-    """
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = user
+        post.save()
+        return JsonResponse(
+            {"post": {"title": post.title, "text": post.text}}, status=CREATED
+        )
 
 
 @require_http_methods(["PUT"])
 def update_post_with_put(request, id):
-    """
-    해당 view 함수를 구현하시오.
+    body = json.loads(request.body)
 
-    Case 1: 성공하는 경우,
-    상태코드 200
+    try:
+        user = User.objects.get(id=body["author"])
+    except:
+        return JsonResponse(
+            {
+                "message": "author를 찾을 수 없습니다.",
+            },
+            status=NOT_FOUND,
+        )
 
-    Case 2: 실패하는 경우, - author가 존재하지 않음
-    Case 3: 실패하는 경우, - post가 존재하지 않음
-    상태코드 404
+    try:
+        post = Post.objects.get(id=id)
+    except:
+        return JsonResponse(
+            {
+                "message": "post를 찾을 수 없습니다.",
+            },
+            status=NOT_FOUND,
+        )
 
-    자세한 조건은 테스트를 확인해보고 파악하자
-
-    FYI, request.body 활용
-    """
+    form = PostForm(body, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = user
+        post.save()
+        return JsonResponse(
+            {"post": {"title": post.title, "text": post.text}}, status=OK
+        )
