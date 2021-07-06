2c51369a50d7d710d1aa9fa757b3004e164ddcf0
JinHoooooou <jinho4744@naver.com>
2021-07-06 16:34:55 +0900

    add remove post url

--- django_girls_tutorial/step2/jinho/djangogirls/blog/api/urls.py
+++ django_girls_tutorial/step2/jinho/djangogirls/blog/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("posts/<int:id>", views.retrieve_post_detail, name="retrieve_post_detail"),
     path("posts/create", views.create_post, name="create_post"),
     path("posts/<int:id>/put/update", views.update_post_with_put, name="update_post_with_put"),
+    path("posts/<int:id>/remove", views.remove_post_with_delete, name="remove_post_with_delete"),
 ]
