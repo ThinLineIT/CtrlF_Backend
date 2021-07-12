628508086b518bc04d1e5f5f9164f9d84e2e7062
jrhong95 <jrhong95@gmail.com>
2021-07-12 17:08:54 +0900

    Add retrieve_comment_list API

--- django_girls_tutorial/step2/sehwa/djangogirls/blog/api/views.py
+++ django_girls_tutorial/step2/sehwa/djangogirls/blog/api/views.py
@@ -4,13 +4,11 @@ from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 def retrieve_post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
-        "published_date"
-    )
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
 
     post_list = [
         {
@@ -43,3 +41,15 @@ def retrieve_post_detail(request, id):
             },
             status=200,
         )
+
+
+def retrieve_comment_list(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        return JsonResponse({"message": "post가 없습니다."}, status=404)
+
+    comments = Comment.objects.filter(post=post, created_date__lte=timezone.now()).order_by("created_date")
+    comment_list = [{"author": comment.author, "text": comment.text} for comment in comments]
+
+    return JsonResponse({"data": comment_list})
