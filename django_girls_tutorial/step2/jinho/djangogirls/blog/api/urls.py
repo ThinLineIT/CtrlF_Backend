ca17e3b79deeab85ae88f29a992e5d9b2e8139b8
JinHoooooou <jinho4744@naver.com>
2021-07-13 14:34:14 +0900

    add create_comment_to_post api url

--- django_girls_tutorial/step2/jinho/djangogirls/blog/api/urls.py
+++ django_girls_tutorial/step2/jinho/djangogirls/blog/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path("posts/", views.retrieve_post_list, name="retrieve_post_list"),
     path("posts/<int:id>", views.retrieve_post_detail, name="retrieve_post_detail"),
     path("posts/create", views.create_post, name="create_post"),
     path("posts/<int:id>/put/update", views.update_post_with_put, name="update_post_with_put"),
+    path("comments/create", views.create_comment_to_post, name="create_comment_to_post"),
 ]
