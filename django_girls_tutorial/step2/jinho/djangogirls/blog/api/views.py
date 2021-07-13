8424277dbd22d3ba1324c27db327234ca9c89c8f
JinHoooooou <jinho4744@naver.com>
2021-07-13 14:36:27 +0900

    add create_comment_to_post method

--- django_girls_tutorial/step2/jinho/djangogirls/blog/api/views.py
+++ django_girls_tutorial/step2/jinho/djangogirls/blog/api/views.py
@@ -1,12 +1,13 @@
 import json
-from http.client import NOT_FOUND, OK
+from http import HTTPStatus
 
 from django.contrib.auth.models import User
 from django.http import JsonResponse, QueryDict
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 
-from blog.models import Post
+from blog.form import CommentForm
+from blog.models import Post, Comment
 
 
 def retrieve_post_list(request):
@@ -87,3 +88,24 @@ def update_post_with_put(request, id):
 
     FYI, request.body 활용
     """
+
+
+@require_http_methods(["POST"])
+def create_comment_to_post(request):
+    body = request.POST
+    comment_form = CommentForm(body)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = body["author"]
+        comment.post = Post.objects.get(id=body["post_id"])
+        comment.text = body["text"]
+        comment.save()
+        return JsonResponse({
+            "comment": {
+                "id": comment.id,
+                "author": comment.author,
+                "post_id": comment.post.id,
+                "text": comment.text,
+            }
+        }, status=HTTPStatus.CREATED)
+    return JsonResponse({"message": "form is invalid"}, status=HTTPStatus.BAD_REQUEST)
