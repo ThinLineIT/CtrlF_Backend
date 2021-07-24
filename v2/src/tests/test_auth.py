4fbdbf4e6cc7af917f6d95c02585a40499969100
TaeHyoungKwon <kth5604@gmail.com>
2021-07-24 11:56:51 +0900

    TH-15 add test code about login should return token

--- v2/src/tests/test_auth.py
+++ v2/src/tests/test_auth.py
@@ -4,8 +4,17 @@ from django.urls import reverse
 
 
 class TestLogin(TestCase):
-    def test_login_should_return_200(self):
+    def setUp(self) -> None:
+        self.c = Client()
         CtrlfUser.objects.create_user(email="test123@naver.com", password="12345")
-        c = Client()
-        response = c.post(reverse("auth:login"), {"email": "test123@naver.com", "password": "12345"})
+
+    def _call_api(self):
+        return self.c.post(reverse("auth:login"), {"email": "test123@naver.com", "password": "12345"})
+
+    def test_login_should_return_200(self):
+        response = self._call_api()
         self.assertEqual(response.status_code, 200)
+
+    def test_login_should_return_token(self):
+        response = self._call_api()
+        self.assertIn("token", response.data)
