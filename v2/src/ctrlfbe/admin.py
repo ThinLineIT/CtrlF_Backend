d85b69a3024f928fffe82af88200e774f06d9d56
TaeHyoungKwon <kth5604@gmail.com>
2021-07-31 17:57:39 +0900

    TH-73 add ctrlbe app and add model about Note

---
+++ v2/src/ctrlfbe/admin.py
@@ -0,0 +1,4 @@
+from ctrlfbe.models import Note
+from django.contrib import admin
+
+admin.site.register(Note)
---
+++ v2/src/ctrlfbe/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Note(models.Model):
+    user = models.ForeignKey("CtrlfUser", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    is_approved = models.BooleanField(default=False)
+    request_contents = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
