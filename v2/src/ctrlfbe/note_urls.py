817aa797dd1eaf01df8d4956d0c9743763eba52f
jrhong95 <jrhong95@gmail.com>
2021-08-12 21:53:27 +0900

    SH-108 Add topic list API and Success test

--- v2/src/ctrlfbe/note_urls.py
+++ v2/src/ctrlfbe/note_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import NoteDetailUpdateDeleteView
+from .views import NoteDetailUpdateDeleteView, TopicListView
 
 app_name = "notes"
 
 urlpatterns = [
     path("<int:note_id>", NoteDetailUpdateDeleteView.as_view(), name="note_detail_update_delete"),
-    # path("<int:note_id>/topics", TopicListView.as_view(), name="topic_list"),
+    path("<int:note_id>/topics", TopicListView.as_view(), name="topic_list"),
 ]
--- v2/src/ctrlfbe/views.py
+++ v2/src/ctrlfbe/views.py
@@ -6,8 +6,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Note
-from .serializers import NoteSerializer
+from .models import Note, Topic
+from .serializers import NoteSerializer, TopicSerializer
 
 
 class NoteDetailUpdateDeleteView(APIView):
@@ -22,4 +22,19 @@ class NoteDetailUpdateDeleteView(APIView):
             return Response({"message": ERR_MSG}, status.HTTP_404_NOT_FOUND)
 
         serializer = NoteSerializer(note)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class TopicListView(APIView):
+    authentication_classes: List[str] = []
+
+    def get(self, request, note_id):
+        ERR_MSG = "노트를 찾을 수 없습니다."
+
+        topics = Topic.objects.filter(note=note_id)
+
+        if not len(topics):
+            return Response({"message": ERR_MSG}, status.HTTP_404_NOT_FOUND)
+
+        serializer = TopicSerializer(topics, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
