07531d4494e57340c647128c9fbf4686a5f634bb
jrhong95 <jrhong95@gmail.com>
2021-08-12 22:50:18 +0900

    SH-104 remove unnecessary annotation

--- v2/src/ctrlfbe/note_urls.py
+++ v2/src/ctrlfbe/note_urls.py
@@ -6,5 +6,4 @@ app_name = "notes"
 
 urlpatterns = [
     path("<int:note_id>", NoteDetailUpdateDeleteView.as_view(), name="note_detail_update_delete"),
-    # path("<int:note_id>/topics", TopicListView.as_view(), name="topic_list"),
 ]
