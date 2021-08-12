bd67e6e8c70416fe8e0e272e3c1fc39f7f27d1bb
jrhong95 <jrhong95@gmail.com>
2021-08-12 20:58:53 +0900

    sh-104 Add Note detail api fail test

--- v2/src/ctrlfbe/views.py
+++ v2/src/ctrlfbe/views.py
@@ -6,8 +6,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Note, Topic
-from .serializers import NoteSerializer, TopicSerializer
+from .models import Note
+from .serializers import NoteSerializer
 
 
 class NoteDetailUpdateDeleteView(APIView):
@@ -18,22 +18,7 @@ class NoteDetailUpdateDeleteView(APIView):
         try:
             note = Note.objects.get(pk=note_id)
         except Note.DoesNotExist:
-            return Response({"message": ERR_MSG}, status.HTTP_400_BAD_REQUEST)
+            return Response({"message": ERR_MSG}, status.HTTP_404_NOT_FOUND)
 
         serializer = NoteSerializer(note)
-        return Response(serializer.data, status.HTTP_200_OK)
-
-
-class TopicListView(APIView):
-    authentication_classes: List[str] = []
-
-    def get(self, request, note_id):
-        ERR_MSG = "노트를 찾을 수 없습니다."
-
-        topics = Topic.objects.filter(note=note_id)
-
-        if not len(topics):
-            return Response({"message": ERR_MSG}, status.HTTP_400_BAD_REQUEST)
-
-        serializer = TopicSerializer(topics, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
