3196b09cf4c9cedc7e86aa869403cb4a20fccbf7
JinHoooooou <jinho4744@naver.com>
2021-08-26 16:03:48 +0900

    JH-23 add fail test on invalid content

--- v2/src/tests/test_note.py
+++ v2/src/tests/test_note.py
@@ -152,3 +152,19 @@ class TestNoteCreate(TestCase):
         # And: Note와 Issue는 생성되지 않는다.
         self.assertEqual(Note.objects.count(), 0)
         self.assertEqual(Issue.objects.count(), 0)
+
+    def test_create_note_should_return_400_when_invalid_content(self):
+        # Given: title과 invalid content가 주어진다.
+        invalid_content = ""
+        request_body = {"title": "test title", "content": invalid_content}
+        # And: 로그인 해서 토큰을 발급받은 상황이다.
+        token = self._login()
+
+        # When: 인증이 필요한 create note api를 호출한다.
+        response = self._call_api(request_body, token)
+
+        # Then: status code는 400을 리턴한다.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And: Note와 Issue는 생성되지 않는다.
+        self.assertEqual(Note.objects.count(), 0)
+        self.assertEqual(Issue.objects.count(), 0)
