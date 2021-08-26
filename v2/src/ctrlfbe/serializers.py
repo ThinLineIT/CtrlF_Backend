e94fce242278ba048c1fa08c29119a5c20b3b76b
JinHoooooou <jinho4744@naver.com>
2021-08-26 15:42:37 +0900

    JH-23 remove OwnerSerializer and ContentRequestSerializer

--- v2/src/ctrlfbe/serializers.py
+++ v2/src/ctrlfbe/serializers.py
@@ -1,25 +1,21 @@
-from ctrlf_auth.models import CtrlfUser
 from rest_framework import serializers
 
-from .models import ContentRequest, Issue, Note, Page, Topic
+from .models import (
+    ContentRequest,
+    CtrlfActionType,
+    CtrlfContentType,
+    Issue,
+    Note,
+    Page,
+    Topic,
+)
 
 
 class NoteListSerializer(serializers.ListSerializer):
     pass
 
 
-class OwnerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CtrlfUser
-        exclude = ["password"]
-        extra_kwargs = {
-            "email": {"validators": [str]},
-        }
-
-
 class NoteSerializer(serializers.ModelSerializer):
-    owners = OwnerSerializer(many=True)
-
     class Meta:
         model = Note
         fields = "__all__"
@@ -27,46 +23,28 @@ class NoteSerializer(serializers.ModelSerializer):
         list_serializer_class = NoteListSerializer
 
     def create(self, validated_data):
-        owner_data = validated_data.pop("owners")
+        owner = validated_data.pop("owners")[0]
         note = Note.objects.create(**validated_data)
-        note.owners.add(CtrlfUser.objects.get(email=owner_data[0].get("email")))
+        note.owners.add(owner)
         return note
 
 
-class ContentRequestSerializer(serializers.ModelSerializer):
-    user = OwnerSerializer()
-
-    class Meta:
-        model = ContentRequest
-        fields = "__all__"
-
-    def create(self, validated_data):
-        user_data = validated_data.pop("user")
-        user = CtrlfUser.objects.get(email=user_data.get("email"))
-        validated_data["user_id"] = user.id
-        content_request = ContentRequest.objects.create(**validated_data)
-
-        return content_request
-
-
 class IssueCreateSerializer(serializers.ModelSerializer):
-    owner = OwnerSerializer()
-    content_request = ContentRequestSerializer()
-
     class Meta:
         model = Issue
         fields = "__all__"
 
     def create(self, validated_data):
-        owner_data = validated_data.pop("owner")
-        owner = CtrlfUser.objects.get(email=owner_data.get("email"))
-
-        content_request_data = validated_data.pop("content_request")
-        content_request = ContentRequest.objects.get(sub_id=content_request_data.get("sub_id"))
-
-        validated_data["content_request"] = content_request
-        validated_data["owner_id"] = owner.id
-        issue = Issue.objects.create(**validated_data)
+        owner = validated_data.pop("owner")
+        content_request_data = {
+            "user": owner,
+            "sub_id": Note.objects.count(),
+            "type": CtrlfContentType.NOTE,
+            "action": CtrlfActionType.CREATE,
+            "reason": "create note",
+        }
+        content_request = ContentRequest.objects.create(**content_request_data)
+        issue = Issue.objects.create(owner=owner, content_request=content_request, **validated_data)
         return issue
 
 
