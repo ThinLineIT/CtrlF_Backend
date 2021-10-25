fdcf632ae9a41d6ef871e6e3e62ccf10f9083391
JinHoooooou <jinho4744@naver.com>
2021-10-25 12:20:41 +0900

    JH-160 Feat: Add test topic create api on fail

--- v2/src/tests/test_topics.py
+++ v2/src/tests/test_topics.py
@@ -49,3 +49,42 @@ class TestTopicCreate(TestCase):
         # And: Issue가 정상적으로 생성된다.
         issue = Issue.objects.all()[0]
         self.assertEqual(issue.content, "test issue content on test topic")
+
+    def test_should_return_400_on_invalid_note_id(self):
+        # Given: invalid한 note_id가 주어졌을 때
+        invalid_note_id = 8954345
+        request_body = {
+            "note_id": invalid_note_id,
+            "title": "test topic title",
+            "content": "test issue content on test topic",
+        }
+        # And: 회원가입된 user정보로 로그인을 해서 토큰을 발급받은 상황
+        token = self._login()
+
+        # When: 인증이 필요한 create topic api를 호출한다.
+        response = self._call_api(request_body, token)
+
+        # Then: status code는 400을 리턴한다.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And: Topic은 생성되지 않는다.
+        self.assertEqual(Topic.objects.count(), 0)
+        # And: Issue도 생성되지 않는다.
+        self.assertEqual(Issue.objects.count(), 0)
+
+    def test_should_return_401_on_unauthorized(self):
+        # Given: topic title과 이슈 내용이 주어진다.
+        request_body = {
+            "note_id": self.note.id,
+            "title": "test topic title",
+            "content": "test issue content on test topic",
+        }
+
+        # When: 인증이 필요한 create topic api를 호출한다.
+        response = self._call_api(request_body)
+
+        # Then: status code는 401을 리턴한다.
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        # And: Topic은 생성되지 않는다.
+        self.assertEqual(Topic.objects.count(), 0)
+        # And: Issue도 생성되지 않는다.
+        self.assertEqual(Issue.objects.count(), 0)
