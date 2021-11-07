bb7ee98bde0a79863661445b01f797e8009aecdf
TaeHyoungKwon <kth5604@gmail.com>
2021-11-07 17:42:24 +0900

    TH-0 Change setting by env

--- v2/src/config/celery.py
+++ v2/src/config/celery.py
@@ -1,8 +1,4 @@
-import os
-
 from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery("config")
 app.conf.broker_url = "redis://localhost:6379/0"
--- v2/src/config/settings/local.py
+++ v2/src/config/settings/local.py
@@ -1,3 +1,9 @@
 from .base import *  # noqa: F403, F401
 
 DEBUG = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+    }
+}
