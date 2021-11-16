d367b9196571893bcc312ea255d6abce296c8578
TaeHyoungKwon <kth5604@gmail.com>
2021-11-16 21:55:14 +0900

    JH-170 Fix: add default value in region for passing test

--- v2/src/config/settings/test.py
+++ v2/src/config/settings/test.py
@@ -16,4 +16,4 @@ S3_BASE_URL = env.str("S3_BASE_URL", default="")
 
 S3_AWS_ACCESS_KEY_ID = env.str("S3_AWS_ACCESS_KEY_ID", default="")
 S3_AWS_SECRET_ACCESS_KEY = env.str("S3_AWS_SECRET_ACCESS_KEY", default="")
-S3_AWS_REGION = env.str("S3_AWS_REGION", default="")
+S3_AWS_REGION = env.str("S3_AWS_REGION", default="north-east-2")
