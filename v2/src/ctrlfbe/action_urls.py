dcb56cdba08f9fa0785e37c82f8fdd341a370c37
TaeHyoungKwon <kth5604@gmail.com>
2022-03-09 19:21:01 +0900

    Feat: Add urls in actions

--- v2/src/ctrlfbe/action_urls.py
+++ v2/src/ctrlfbe/action_urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import ImageUploadView, IssueApproveView, IssueCloseView, IssueDeleteView
+from .views import (
+    ImageUploadView,
+    IssueApproveView,
+    IssueCloseView,
+    IssueDeleteView,
+    IssueRejectView,
+    IssueUpdateView,
+)
 
 app_name = "actions"
 
@@ -8,5 +15,7 @@ urlpatterns = [
     path("issue-approve/", IssueApproveView.as_view(), name="issue_approve"),
     path("issue-delete/", IssueDeleteView.as_view(), name="issue_delete"),
     path("issue-close/", IssueCloseView.as_view(), name="issue_close"),
+    path("issue-update/", IssueUpdateView.as_view(), name="issue_update"),
+    path("issue-reject/", IssueRejectView.as_view(), name="issue_reject"),
     path("images/", ImageUploadView.as_view(), name="upload_images"),
 ]
